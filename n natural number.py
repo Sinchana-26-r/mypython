a81559669c2f664e7341530cbd64b2e3f0954d14
Sinchana-26-r <sinchanarsinchana95@gmail.com>
2025-11-12 11:19:56 +0530

    Create n natural number.py

---
+++ n natural number.py
@@ -0,0 +1,8 @@
+n = int(input("enter the number :"))
+sum=0
+s = 1
+while s <= n:
+    sum=sum + s
+    s = s + 1
+print("the sum of first",n, "natural numbers is:",sum)enter the number :4
+the sum of first 4 natural numbers is: 10
