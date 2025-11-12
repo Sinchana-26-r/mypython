f116feefc257865b34069f5301586a2757366931
Sinchana-26-r <sinchanarsinchana95@gmail.com>
2025-11-12 09:36:27 +0530

    Create divisibility test.py

---
+++ divisibility test.py
@@ -0,0 +1,10 @@
+#divisibility test
+num=float(input("enter the number"))
+if num%3==0:
+  print(num,"is divisible by 3")
+elif num%5==0:
+  print(num,"is divisible by 5")
+else:
+  print(num,"is not divisible by both 3 and 5")
+    enter the number6
+6.0 is divisible by 3
