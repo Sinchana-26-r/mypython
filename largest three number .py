c96b41d8803a7331c12d72c9130e1d9cd0f7a9eb
Sinchana-26-r <sinchanarsinchana95@gmail.com>
2025-11-12 09:41:14 +0530

    Create largest three number .py

---
+++ largest three number .py
@@ -0,0 +1,13 @@
+#largest three number
+num1=int(input("enter the first number"))
+num2=int(input("enter the second number"))
+num3=int(input("enter the thrid number"))
+if num1<num2:
+   print(num2 ,"the largest number is")
+elif num2<num3:
+    print(num3, "the largest number is")
+else:
+     print(num1,"the largest number is")enter the first number10
+enter the second number26
+enter the thrid number926
+26 the largest number is
