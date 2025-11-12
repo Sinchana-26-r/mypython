1549afe89ed06b91f6e967ffbbd3778e46df814e
Sinchana-26-r <sinchanarsinchana95@gmail.com>
2025-11-12 11:17:34 +0530

    Create odd and even .py

---
+++ odd and even .py
@@ -0,0 +1,31 @@
+for sinchana in range(1,26):
+  if sinchana%4==0:
+   print("even")
+  else:
+   print("odd")
+
+odd
+odd
+odd
+even
+odd
+odd
+odd
+even
+odd
+odd
+odd
+even
+odd
+odd
+odd
+even
+odd
+odd
+odd
+even
+odd
+odd
+odd
+even
+odd
