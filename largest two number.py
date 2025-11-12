ee06cad3cc709155b93cfbc5deb7ab06cc8229c1
Sinchana-26-r <sinchanarsinchana95@gmail.com>
2025-11-12 09:39:58 +0530

    Create largest two number.py

---
+++ largest two number.py
@@ -0,0 +1,11 @@
+# largest two number
+num1=float(input("enter the first number"))
+num2=float(input("enter the second number"))
+if num1>num2:
+   print(num1 ,"the largest number is ")
+elif num1<num2:
+    print(num2,"the largest number is")
+else:
+       print("both the number are equal")enter the first number4
+enter the second number6
+6.0 the largest number is
