afb9dc65fac2fcf0840b2f96a614982ba5883003
Sinchana-26-r <sinchanarsinchana95@gmail.com>
2025-11-12 11:36:21 +0530

    Create mutiplcation table .py

---
+++ mutiplcation table .py
@@ -0,0 +1,17 @@
+#mupltication tablemy name is: sinchana r my age is: 18 my college is: shridevi my departamen
+num = int(input("enter the number"))
+for s in range(1,11):
+   print(f"{9} x {s} = {9*s}")
+
+enter the number9
+9 x 1 = 9
+9 x 2 = 18
+9 x 3 = 27
+9 x 4 = 36
+9 x 5 = 45
+9 x 6 = 54
+9 x 7 = 63
+9 x 8 = 72
+9 x 9 = 81
+9 x 10 = 90
+
