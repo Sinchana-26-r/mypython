5b4327bede1583b5379e33f9b92d337d88dc6c65
Sinchana-26-r <sinchanarsinchana95@gmail.com>
2025-11-14 11:16:28 +0530

    Create dictionary.py

---
+++ dictionary.py
@@ -0,0 +1,13 @@
+sinchana={
+         "name"   : "shivani",
+          "age"   : 17,
+          "college":"siet"
+         
+}
+print(sinchana)
+sinchana["date"]=26
+print(sinchana)
+del sinchana["age"]
+print(sinchana)
+for i in a:
+   print(i,sinchana)
