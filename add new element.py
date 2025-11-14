993d96a3f7f67eb870e5da5316e5761ece6030d9
Sinchana-26-r <sinchanarsinchana95@gmail.com>
2025-11-14 15:40:38 +0530

    Create add new element.py

---
+++ add new element.py
@@ -0,0 +1,14 @@
+#add a new element
+good={
+      "name"   :"appu",
+      "age"    : 18,
+      "course" : "aiml"
+
+
+}
+print(good)
+good["date"]=9
+print(good)
+del good["course"]
+print(good)
+
